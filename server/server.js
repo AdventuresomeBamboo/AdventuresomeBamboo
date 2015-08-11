62db0f7c60ab03ec8abb7a603db4998834cd2c6d
ceditoph <CR.patterson2000@gmail.com>
2015-08-11 10:35:04 -0700

    Incorporating server

---
+++ server/server.js
@@ -0,0 +1,39 @@
+//Lets require/import the HTTP module
+var http = require('http');
+//Requiring express JS for easy routing
+var express = require('express');
+//
+var path = require('path')
+//Setting router as variable to use express methods
+var app = express();
+//Creating the router
+var router = express.Router();
+
+//Lets define a port we want to listen to
+const PORT=5678; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end();
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+app.listen(PORT, function(){
+    //Callback triggered when server is successfully listening.
+    console.log("Server listening on: http://localhost:%s", PORT);
+})
+
+// Routing
+// app.use('/', express.static(__dirname + '/'));
+app.get('/',function(req, res){
+  res.sendFile(path);
+})
+
+app.post('/',function(req, res){
+  res.on('data', function(chunk){
+    console.log(chunk)
+  })
+})
