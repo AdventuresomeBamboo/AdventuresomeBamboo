6277e6b5d2b08f5dd88cf6c311d722f451078f8a
ceditoph <CR.patterson2000@gmail.com>
2015-08-13 15:07:04 -0700

    Async solved! Using external utils caused too much of a problem. Using async within the router with helper functions.

--- server/server.js
+++ server/server.js
@@ -6,51 +6,28 @@ var router = require('../utils/router.js'); // Request handling-routing
 
 /*********************** Routing ****************************/
 //Handled by app.js
-var openPort = 3456;
-var lockPort = 3911;
+var openPort = 5678;
+var lockPort = 5911;
 http.createServer(app).listen(openPort, function(){
-  console.log('Listening on port',openPort)
+  console.log('Server listening on port',openPort)
 }); // <-- initialize server for http
 https.createServer(app).listen(lockPort) // <-- initialize server for https
 
-app.get('*',function(req, res){
-var http = require('http'); // Require/import the HTTP module
-var app = require('express')(); // Defining server
-var router = require('../utils/router.js'); // Request handling-routing
-
-const PORT=6789; // Port we want to listen to
-var server = http.createServer(); //Create a server
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening.
-    console.log("Server listening on: http://localhost:%s", PORT);
-  })
 /*********************** Routing ****************************/
 //Handled by router.js
 
 app.get('/',function(req, res){
-  console.log('get request made')
-  var req = req;
-  var res = res;
-  router.requestHandler('/', req, res)
+  router.requestHandler('/', req, res);
 });
 
 app.post('/state',function(req, res){
-  var req = req;
-  var res = res;
-  router.requestHandler('/state', req, res)
+  router.requestHandler('/state', req, res);
 });
 
 app.post('/cropType',function(req, res, next){
-  var req = req;
-  var res = res;
-  router.requestHandler('/cropType', req, res)
+  router.requestHandler('/cropType', req, res);
 });
 
 app.post('/crop',function(req, res, next){
-  var req = req;
-  var res = res;
-  router.requestHandler('/crop', req, res)
-
+  router.requestHandler('/crop', req, res);
 });
--- utils/router.js
+++ utils/router.js
@@ -2,8 +2,9 @@ var express = require('express');//Requiring express JS for easy routing
 var router = express.Router(); //Creating the router
 var url = require('url'); // URL handling
 var async = require('async-series'); // Async JS for handling async operations
-var path = require('path');
-var utils = require('./utils.js'); //Utility-helper functions
+var path = require('path'); // For resolving path names
+var request = require('request'); // Request JS for simple API calls
+var currentYear = new Date().getFullYear(); //Dynamically set current year
 
 var root = express.static(__dirname + '/../client');// Set root path for static routes
 
@@ -16,53 +17,22 @@ module.exports.requestHandler = function (route, req, res){
   }
   else if(route === '/state'){
   //Post request for the crop types name
-    state = (url.parse(req.url).query).toUpperCase();
-    //*******ATTENTION***********\\ need to send back all crop types
-    res.writeHead(200);
-    res.write(cropTypes);
-    res.end();
+    state = 'TEXAS'//(url.parse(req.url).query).toUpperCase();
+    getCropTypes(state, req, res);
   }
   else if(route === '/cropType'){
   //Post request for the cropType
-    var cropList;
-    cropType = //(url.parse(req.url).query).toUpperCase();
-    async([
-      function(done){
-        cropList = utils.getCropNames('IDAHO', 'VEGETABLES');
-        done(false);  
-      },
-      function(done){
-        console.log('cropList received')
-        done(false);
-      }],
-      function(err){
-        if(err){console.log('Following error ocurred : ',err); return};
-        console.log('...processing...')
-        console.log(cropList);
-        //res.writeHead(200);
-        //res.write(cropList)
-        //res.end();
-      }
-    );
+  var cropList;
+    cropType = 'VEGETABLES'//(url.parse(req.url).query).toUpperCase();
+    getCropNames(state, cropType, req, res);
   }
   else if(route === '/crop'){
   //Post request for the crop name
-    var production = {};
-    var year = new Date().getFullYear()-1;
-    crop = (url.parse(req.url).query).toUpperCase();
+  var production = {};
+  var year = new Date().getFullYear()-1;
+  crop = 'TOMATOES'//(url.parse(req.url).query).toUpperCase();
     //need an async loop. BUT dont know how
-    async([
-      //first call without looping
-      function(cb){
-        production[year] = utils.showCropInfo(state, cropType, crop, year);
-        cb();
-      },
-      function(cb){
-        res.writeHead(200);
-        res.end(production);
-        cb();
-      }
-    ]);
+    showCropInfo(state, cropType, crop, year, req, res);
   } else {
     console.log('invalid route');
     res.writeHead(404);
@@ -70,17 +40,72 @@ module.exports.requestHandler = function (route, req, res){
   }
 };
 
-/*HELPERS*/
-var worker = function (func, cb){
-  func.call(this, arguments);
-  return cb();
+/*********************** \Functions/ ***********************/
+
+var getCropTypes = function (state, req, res){
+  link = 'http://nass-api.azurewebsites.net/api/api_get?source_desc=SURVEY&sector_desc=CROPS&agg_level_desc=STATE&state_name='+state+'&year=2014&statisticcat_desc=PRODUCTION&class_desc=ALL%20CLASSES&domain_desc=TOTAL&freq_desc=ANNUAL&util_practice_desc=ALL%20UTILIZATION%20PRACTICES';
+  async([
+    function(done){
+      request.get(link, function (error, response, body){
+        types = JSON.parse(body).data
+      })
+      done(false);
+    },
+    function(done){
+      console.log('processing...');
+      done(false);
+    }],
+    function(err){
+      if(err){console.log('Following error ocurred : ',err); return};
+      console.log('Finished getting crop types');
+      res.writeHead(200);
+      res.write(types);
+      res.end();
+    });
 };
 
-var waiter = function (func){
-  console.log(func)
-  worker(func, function(){
-    console.log('processing complete');
-  });
+var getCropNames = function (state, cropType, req, res){
+  var crops; // <-- holder for the crop names that will be passed in
+  link = 'http://nass-api.azurewebsites.net/api/api_get?source_desc=SURVEY&sector_desc=CROPS&group_desc='+cropType+'&agg_level_desc=STATE&state_name='+state+'&year=2014&statisticcat_desc=PRODUCTION&class_desc=ALL%20CLASSES&domain_desc=TOTAL&freq_desc=ANNUAL&util_practice_desc=ALL%20UTILIZATION%20PRACTICES&unit_desc=CWT';
+  // ^-- Above is the link for the API request check the vars in the string...
+  async([ //<-- handling of asynchronous calls
+    function(done){
+      request.get(link, function (error, response, body){ // <-- initiates connection to API server
+        crops = JSON.parse(body).data;
+        done(false);
+      }) 
+    },
+    function(done){
+      console.log('processing...');
+      done(false);
+    }],
+    function(err){
+      if(err){console.log('Following error ocurred : ',err); return};
+      console.log('Finished getting crop names!')
+      return crops; //return it
+    });
 };
 
-module.exports.requestHandler('/cropType')
+var showCropInfo = function (state, cropType, crop, year, req, res){
+  var year = year || 2012;
+  var production = {}; // <-- holder for the production values that will be passed in
+  var data = ''; // <-- data hold for parsing
+  link = 'http://nass-api.azurewebsites.net/api/api_get?source_desc=SURVEY&sector_desc=CROPS&group_desc='+cropType+'&agg_level_desc=STATE&year='+year+'&state_name='+state+'&commodity_desc='+crop;
+  // ^-- Above is the link for the API request check the vars in the string...
+  async([//<-- handling of asynchronous calls
+    function(done){
+      request.get(link, function (err, response, body){// <-- initiates connection to API server
+        production[year] = JSON.parse(body).data;
+      });
+      done(false);
+    },
+    function(done){
+      console.log('getting crop info...')
+      done(false);
+    }],
+    function(err){
+      if(err){console.log('Following error ocurred : ',err); return};
+      console.log('Finished getting crop names!')
+      return production;
+    });
+};
